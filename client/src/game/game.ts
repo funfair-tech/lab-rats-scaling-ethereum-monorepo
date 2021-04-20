9a3462c2ee8bece8ab8b4a50cd6ff0b28bd5ce9f
Paul Jackman <paul@funfair.io>
2021-04-20 17:34:15 +0100

    game: add constructor and simplify import

--- client/src/game/game.ts
+++ client/src/game/game.ts
@@ -1,18 +1,23 @@
 // import { FFEngine } from '@funfair/engine';
-const Engine = require('@funfair/engine');
+const FFEngine = require('@funfair/engine').FFEngine;
 
 export class Game {
+
+  constructor() {
+    console.log('Creating Labrats Game');
+  }
+
   public report(): string {
     return 'Canvas ready for duty';
   }
 
   public getEngineVersion(): string {
-    console.log('Engine: ', Engine);
-    return Engine.FFEngine.instance.GetVersion();
+    console.log('Engine: ', FFEngine);
+    return FFEngine.instance.GetVersion();
   }
 
   public initEngine(): void {
-    console.log('Engine: ', Engine);
-    Engine.FFEngine.instance.Init();
+    console.log('Engine: ', FFEngine);
+    FFEngine.instance.Init();
   }
 }
