f5e406f4246f7bb3593439acc5de176da8e701a6
DaneWiid1 <dane@funfair.io>
2021-04-21 15:57:49 +0100

    feat(client): hook engine init into effect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,24 @@
 import './App.scss';
-import { Button } from './components/button/button';
 import { Canvas } from './components/canvas/canvas';
 import AccountBar from './containers/accountBar/accountBar';
 import { Game } from './game/game';
-import { connect } from 'react-redux';
-import { setAddress } from './store/actions/user.actions';
+import { useEffect, useMemo } from 'react';
 
 function App() {
-  const game = new Game();
+
+  const game = useMemo(() => new Game(), []);
+
+  useEffect(() => {
+    console.log('initialising game...');
+    game.initEngine();
+  }, [game]);
 
   return (
     <div className="app">
-      <header className="app__header">
         <AccountBar title='Lab Rats'/>
-
         <Canvas />
-
-        <span>Engine version: {game.getEngineVersion()}</span>
-
-        <section className="app__debugButtons">
-          <Button onClick={() => game.initEngine()}>init engine</Button>
-        </section>
-
-      </header>
-
     </div>
   );
 }
 
-
-const mapDispatchToProps = (dispatch: Function) => {
-  return {
-    setAddress: (address: string) => dispatch(setAddress(address)),
-  };
-};
-
-const connector = connect(null, mapDispatchToProps);
-
-
-export default connector(App);
+export default App;
