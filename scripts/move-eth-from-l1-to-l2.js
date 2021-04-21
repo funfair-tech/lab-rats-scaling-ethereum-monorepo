ffeb2c8fdb08c362254f25d616026ef993468c43
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-04-21 11:22:04 +0100

    move eth from l1 to l2

--- scripts/move-eth-from-l1-to-l2.js
+++ scripts/move-eth-from-l1-to-l2.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { Contract, Wallet } = require('ethers');
 const { JsonRpcProvider } = require('@ethersproject/providers');
 
-const KOVAN_CONTRACT_ADDRESS = '0x0E8917aF9eB7812c7819EF4e80D2217679d11324';
+const KOVAN_CONTRACT_ADDRESS = '0xf3902e50dA095bD2e954AB320E8eafDA6152dFDa';
 const ABI =
   '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_from","type":"address"},{"indexed":false,"internalType":"address","name":"_to","type":"address"},{"indexed":false,"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"DepositInitiated","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_to","type":"address"},{"indexed":false,"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"WithdrawalFinalized","type":"event"},{"inputs":[],"name":"deposit","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"_to","type":"address"}],"name":"depositTo","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"finalizeWithdrawal","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getFinalizeDepositL2Gas","outputs":[{"internalType":"uint32","name":"","type":"uint32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_libAddressManager","type":"address"},{"internalType":"address","name":"_ovmEth","type":"address"}],"name":"initialize","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"libAddressManager","outputs":[{"internalType":"contract Lib_AddressManager","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"messenger","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"ovmEth","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_name","type":"string"}],"name":"resolve","outputs":[{"internalType":"address","name":"_contract","type":"address"}],"stateMutability":"view","type":"function"},{"stateMutability":"payable","type":"receive"}]';
 
@@ -12,7 +12,7 @@ async function main() {
   const l1RpcProvider = new JsonRpcProvider(
     'https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'
   );
-  const l2RpcProvider = new JsonRpcProvider('https://kovan.optimism.io');
+  // const l2RpcProvider = new JsonRpcProvider('https://kovan.optimism.io');
 
   const key = process.env.YOUR_PRIVATE_KEY;
   const l1Wallet = new Wallet(key, l1RpcProvider);
@@ -22,8 +22,6 @@ async function main() {
   const response = await contract.deposit({ value: '10000000000000' });
   console.log(response);
   await response.wait();
-
-  // console.log(await l2Wallet.getBalance());
 }
 
 main()
