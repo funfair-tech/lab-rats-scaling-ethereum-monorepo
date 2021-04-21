145b59dc91e03fc72f900d6fe21895d3ad88cf26
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-04-21 12:32:52 +0100

    add kovan for now

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -20,6 +20,14 @@ module.exports = {
       gasPrice: 0,
       ovm: true, // This sets the network as using the ovm and ensure contract will be compiled against that.
     },
+    kovan: {
+      url: 'https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+      accounts: [process.env.YOUR_PRIVATE_KEY],
+      // This sets the gas price to 0 for all transactions on L2. We do this
+      // because account balances are not automatically initiated with an ETH
+      // balance (yet, sorry!).
+      gasPrice: 0,
+    },
   },
   solidity: '0.8.0',
   ovm: {
