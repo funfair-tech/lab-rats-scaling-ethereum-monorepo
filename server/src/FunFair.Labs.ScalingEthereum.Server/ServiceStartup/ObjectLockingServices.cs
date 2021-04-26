f18c89290ebfe6c27e2be9bfc39819cfd0b42232
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-04-26 17:42:46 +0100

    More object locking/unlocking

--- server/src/FunFair.Labs.ScalingEthereum.Server/ServiceStartup/ObjectLockingServices.cs
+++ server/src/FunFair.Labs.ScalingEthereum.Server/ServiceStartup/ObjectLockingServices.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using FunFair.Common.ObjectLocking.Data.Interfaces;
 using FunFair.Ethereum.DataTypes.Primitives;
+using FunFair.Labs.ScalingEthereum.DataTypes.Primitives;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -10,19 +11,21 @@ namespace FunFair.Labs.ScalingEthereum.Server.ServiceStartup
     internal static class ObjectLockingServices
     {
         private static IObjectLockDataManager<EthereumAddress>? _gameManagerLockDataManager;
+        private static IObjectLockDataManager<GameRoundId>? _gameRoundLockDataManager;
         private static ILogger<Startup>? _logger;
 
         public static Task ClearLocksAsync()
         {
-            return Task.WhenAll(ClearGameManagerLockAsync());
+            return Task.WhenAll(ClearGameManagerLockAsync(), ClearGameRoundLockAsync());
         }
 
         public static Task ClearLocksAsync(IServiceProvider serviceProvider, ILogger<Startup> logger)
         {
             _logger = logger;
             _gameManagerLockDataManager = serviceProvider.GetRequiredService<IObjectLockDataManager<EthereumAddress>>();
+            _gameRoundLockDataManager = serviceProvider.GetRequiredService<IObjectLockDataManager<GameRoundId>>();
 
-            return Task.WhenAll(ClearGameManagerLockAsync());
+            return ClearLocksAsync();
         }
 
         private static Task ClearGameManagerLockAsync()
@@ -30,6 +33,13 @@ namespace FunFair.Labs.ScalingEthereum.Server.ServiceStartup
             _logger?.LogInformation("Clearing locks for game manager");
 
             return _gameManagerLockDataManager?.ClearAllLocksAsync() ?? Task.CompletedTask;
+        }
+
+        private static Task ClearGameRoundLockAsync()
+        {
+            _logger?.LogInformation("Clearing locks for game round");
+
+            return _gameRoundLockDataManager?.ClearAllLocksAsync() ?? Task.CompletedTask;
         }
     }
 }
