896440aeb0e6372afe80be8ca767b89b378d9e29
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-04-26 18:48:23 +0100

    Linked up events on web socket subscription to inform the client what the games if any are in progress

--- server/src/FunFair.Labs.ScalingEthereum.ServiceInterface.Hub/AuthenticatedHub.cs
+++ server/src/FunFair.Labs.ScalingEthereum.ServiceInterface.Hub/AuthenticatedHub.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using FunFair.Common.Environment;
+using FunFair.Ethereum.Blocks.Interfaces;
 using FunFair.Ethereum.DataTypes;
 using FunFair.Ethereum.DataTypes.Primitives;
 using FunFair.Ethereum.Networks.Interfaces;
 using FunFair.Labs.ScalingEthereum.Authentication;
+using FunFair.Labs.ScalingEthereum.Data.Interfaces.GameRound;
 using FunFair.Labs.ScalingEthereum.DataTypes.Primitives;
+using FunFair.Labs.ScalingEthereum.Logic.Games;
+using FunFair.Labs.ScalingEthereum.Logic.Games.EventHandlers;
 using FunFair.Labs.ScalingEthereum.Logic.Players;
 using FunFair.Labs.ScalingEthereum.ServiceInterface.Hub.RateLimit;
 using FunFair.Labs.ScalingEthereum.ServiceInterface.Hub.Subscription;
@@ -22,8 +27,12 @@ namespace FunFair.Labs.ScalingEthereum.ServiceInterface.Hub
     [Authorize(Roles = Role.Dapp)]
     public sealed class AuthenticatedHub : HubBase
     {
+        private readonly IEthereumBlockStatus _ethereumBlockStatus;
+        private readonly IGameRoundDataManager _gameRoundDataManager;
+        private readonly IGameRoundTimeCalculator _gameRoundTimeCalculator;
         private readonly IPlayerCountManager _playerCountManager;
         private readonly IRateLimiter _rateLimiter;
+        private readonly IStartRoundGameHistoryBuilder _startRoundGameHistoryBuilder;
 
         /// <summary>
         ///     Constructor
@@ -33,6 +42,10 @@ namespace FunFair.Labs.ScalingEthereum.ServiceInterface.Hub
         /// <param name="environment">Execution environment</param>
         /// <param name="subscriptionManager">Subscription manager</param>
         /// <param name="playerCountManager">Player counter.</param>
+        /// <param name="gameRoundDataManager">Game Round Manager.</param>
+        /// <param name="gameRoundTimeCalculator">Game round time calculator.</param>
+        /// <param name="startRoundGameHistoryBuilder">Game Round history builder.</param>
+        /// <param name="ethereumBlockStatus">Ethereum block status.</param>
         /// <param name="rateLimiter">Rate limiter</param>
         /// <param name="logger">Logger</param>
         public AuthenticatedHub(IEthereumNetworkRegistry ethereumNetworkRegistry,
@@ -40,11 +53,19 @@ namespace FunFair.Labs.ScalingEthereum.ServiceInterface.Hub
                                 ExecutionEnvironment environment,
                                 ISubscriptionManager subscriptionManager,
                                 IPlayerCountManager playerCountManager,
+                                IGameRoundDataManager gameRoundDataManager,
+                                IGameRoundTimeCalculator gameRoundTimeCalculator,
+                                IStartRoundGameHistoryBuilder startRoundGameHistoryBuilder,
+                                IEthereumBlockStatus ethereumBlockStatus,
                                 IRateLimiter rateLimiter,
                                 ILogger<AuthenticatedHub> logger)
             : base(ethereumNetworkRegistry: ethereumNetworkRegistry, groupNameGenerator: groupNameGenerator, environment: environment, subscriptionManager: subscriptionManager, logger: logger)
         {
             this._playerCountManager = playerCountManager ?? throw new ArgumentNullException(nameof(playerCountManager));
+            this._gameRoundDataManager = gameRoundDataManager ?? throw new ArgumentNullException(nameof(gameRoundDataManager));
+            this._gameRoundTimeCalculator = gameRoundTimeCalculator ?? throw new ArgumentNullException(nameof(gameRoundTimeCalculator));
+            this._startRoundGameHistoryBuilder = startRoundGameHistoryBuilder ?? throw new ArgumentNullException(nameof(startRoundGameHistoryBuilder));
+            this._ethereumBlockStatus = ethereumBlockStatus ?? throw new ArgumentNullException(nameof(ethereumBlockStatus));
             this._rateLimiter = rateLimiter ?? throw new ArgumentNullException(nameof(rateLimiter));
         }
 
@@ -54,7 +75,7 @@ namespace FunFair.Labs.ScalingEthereum.ServiceInterface.Hub
         /// <param name="networkName">The network id</param>
         /// <returns></returns>
         /// <exception cref="ArgumentOutOfRangeException"></exception>
-        [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Called by web socket client")]
+        [SuppressMessage(category: "ReSharper", checkId: "UnusedMember.Global", Justification = "Called by web socket client")]
         public async Task SubscribeAsync(string networkName)
         {
             EthereumNetwork network = this.VerifyNetwork(networkName);
@@ -73,6 +94,50 @@ namespace FunFair.Labs.ScalingEthereum.ServiceInterface.Hub
             int playerCount = await this._playerCountManager.AddPlayerAsync(playerId: this.Context.ConnectionId, newNetwork: network);
 
             await this.Clients.Caller.PlayersOnline(playerCount);
+
+            IReadOnlyList<GameRound> games = await this._gameRoundDataManager.GetAllRunningAsync(network);
+
+            bool anyPublished = false;
+            INetworkBlockHeader? networkBlockHeader = this._ethereumBlockStatus.GetLatestBlockRetrievedOnNetwork(network);
+
+            if (networkBlockHeader != null)
+            {
+                foreach (GameRound game in games)
+                {
+                    IReadOnlyList<string> gameHistory = await this.GetGameHistoryAsync(game: game);
+
+                    int timeLeft = this._gameRoundTimeCalculator.CalculateTimeLeft(game);
+
+                    await this.Clients.Caller.GameRoundStarted(roundId: game.GameRoundId,
+                                                               timeLeftInSeconds: timeLeft,
+                                                               blockNumber: game.BlockNumberCreated,
+                                                               (int) GameRoundParameters.InterGameDelay.TotalSeconds);
+
+                    await this.Clients.Caller.History(gameHistory);
+
+                    anyPublished = true;
+                }
+            }
+
+            if (!anyPublished)
+            {
+                GameRound? gameRound = await this._gameRoundDataManager.GetLastCompletedForNetworkAsync(network);
+
+                if (gameRound != null && networkBlockHeader != null)
+                {
+                    IReadOnlyList<string> gameHistory = await this.GetGameHistoryAsync(game: gameRound);
+
+                    int timeUntilNextRound = this._gameRoundTimeCalculator.CalculateSecondsUntilNextRound(gameRound);
+
+                    await this.Clients.Caller.LastGameRoundEnded(gameRoundId: gameRound.GameRoundId, startBlockNumber: gameRound.BlockNumberCreated, timeToNextRound: timeUntilNextRound);
+
+                    await this.Clients.Caller.History(gameHistory);
+                }
+                else
+                {
+                    await this.Clients.Caller.NoGamesAvailable();
+                }
+            }
         }
 
         /// <summary>
@@ -105,6 +170,14 @@ namespace FunFair.Labs.ScalingEthereum.ServiceInterface.Hub
             int playerCount = await this._playerCountManager.RemovePlayerAsync(this.Context.ConnectionId);
 
             await this.Clients.Caller.PlayersOnline(playerCount);
+        }
+
+        private async Task<IReadOnlyList<string>> GetGameHistoryAsync(GameRound game)
+        {
+            IReadOnlyList<GameHistory> history = await this._gameRoundDataManager.GetHistoryAsync(gameContractAddress: game.GameContract.Address,
+                                                                                                  maxHistoryItems: NotificationConfiguration.MaxHistoryItems);
+
+            return this._startRoundGameHistoryBuilder.Build(history);
         }
     }
 }
