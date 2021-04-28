86357207e5d29cf7d91c5b2a78dee507be456eaa
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-04-28 15:26:32 +0100

    Changed round activation date to use machine time rather than block time

--- server/src/FunFair.Labs.ScalingEthereum.Logic/Games/EventHandlers/StartGameRoundEventHandler.cs
+++ server/src/FunFair.Labs.ScalingEthereum.Logic/Games/EventHandlers/StartGameRoundEventHandler.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
+using FunFair.Common.Interfaces;
 using FunFair.Common.ObjectLocking;
 using FunFair.Ethereum.DataTypes;
 using FunFair.Ethereum.DataTypes.Primitives;
@@ -19,6 +20,7 @@ namespace FunFair.Labs.ScalingEthereum.Logic.Games.EventHandlers
     [SuppressMessage(category: "ReSharper", checkId: "ClassNeverInstantiated.Global", Justification = "Instantiated by DI")]
     public sealed class StartGameRoundEventHandler : GameRoundEventHandlerBase<StartGameRoundEventOutput>
     {
+        private readonly IDateTimeSource _dateTimeSource;
         private readonly IGameRoundTimeCalculator _gameRoundTimeCalculator;
         private readonly IGameStatisticsPublisher _gameStatisticsPublisher;
 
@@ -29,16 +31,19 @@ namespace FunFair.Labs.ScalingEthereum.Logic.Games.EventHandlers
         /// <param name="gameRoundDataManager">Game Round Data manager</param>
         /// <param name="gameRoundLockManager">Game Round Lock manager.</param>
         /// <param name="gameRoundTimeCalculator">Game Round time calculator.</param>
+        /// <param name="dateTimeSource">Source of time.</param>
         /// <param name="logger">Logging.</param>
         public StartGameRoundEventHandler(IGameStatisticsPublisher gameStatisticsPublisher,
                                           IGameRoundDataManager gameRoundDataManager,
                                           IObjectLockManager<GameRoundId> gameRoundLockManager,
                                           IGameRoundTimeCalculator gameRoundTimeCalculator,
+                                          IDateTimeSource dateTimeSource,
                                           ILogger<StartGameRoundEventHandler> logger)
             : base(gameRoundDataManager: gameRoundDataManager, gameRoundLockManager: gameRoundLockManager, logger: logger)
         {
             this._gameStatisticsPublisher = gameStatisticsPublisher ?? throw new ArgumentNullException(nameof(gameStatisticsPublisher));
             this._gameRoundTimeCalculator = gameRoundTimeCalculator ?? throw new ArgumentNullException(nameof(gameRoundTimeCalculator));
+            this._dateTimeSource = dateTimeSource ?? throw new ArgumentNullException(nameof(dateTimeSource));
         }
 
         /// <inheritdoc />
@@ -63,11 +68,14 @@ namespace FunFair.Labs.ScalingEthereum.Logic.Games.EventHandlers
                 return true;
             }
 
+            // n.b. can't use the network time as blocks are not regular
+            DateTime now = this._dateTimeSource.UtcNow();
+
             GameRound newRoundState =
                 new(gameRoundId: gameRound.GameRoundId, network: gameRound.Network, gameManagerContract: gameRound.GameManagerContract, createdByAccount: gameRound.CreatedByAccount, gameContract:
                     gameRound.GameContract, seedCommit: gameRound.SeedCommit, seedReveal: gameRound.SeedReveal, status: GameRoundStatus.STARTED, roundDuration: gameRound.RoundDuration,
-                    roundTimeoutDuration: gameRound.RoundTimeoutDuration, dateCreated: gameRound.DateCreated, dateUpdated: networkBlockHeader.Timestamp, dateStarted: networkBlockHeader.Timestamp,
-                    dateClosed: null, blockNumberCreated: networkBlockHeader.Number);
+                    roundTimeoutDuration: gameRound.RoundTimeoutDuration, dateCreated: gameRound.DateCreated, dateUpdated: networkBlockHeader.Timestamp, dateStarted: now, dateClosed: null,
+                    blockNumberCreated: networkBlockHeader.Number);
 
             await this.GameRoundDataManager.ActivateAsync(activationTime: newRoundState.DateStarted!.Value,
                                                           gameRoundId: newRoundState.GameRoundId,
