40205ea9476b7a5f1295ff72a1a6ed8117d69f84
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-04-28 13:41:41 +0100

    Reduced sweep to 1 sec so triggered every second as well as every block

--- server/src/FunFair.Labs.ScalingEthereum.Logic/Games/BackgroundServices/Services/EndGameBettingBackgroundService.cs
+++ server/src/FunFair.Labs.ScalingEthereum.Logic/Games/BackgroundServices/Services/EndGameBettingBackgroundService.cs
@@ -36,7 +36,7 @@ namespace FunFair.Labs.ScalingEthereum.Logic.Games.BackgroundServices.Services
                    latestBlockRetriever: latestBlockRetriever,
                    processUnchangedBlocks: false,
                    dateTimeSource: dateTimeSource,
-                   sweepInterval: TimeSpan.FromSeconds(30),
+                   sweepInterval: TimeSpan.FromSeconds(1),
                    blockTriggeredService: new BackgroundService(endGameBettingService),
                    logger: logger)
         {
