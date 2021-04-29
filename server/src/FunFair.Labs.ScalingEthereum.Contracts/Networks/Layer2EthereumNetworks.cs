143fce107fe18394c441206907bf34a0343c85f6
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-04-29 19:02:00 +0100

    Updated Ethereum packages to include better support for optimism where blocks are not on a regular frequency, but created on demand

--- server/src/FunFair.Labs.ScalingEthereum.Contracts/Networks/Layer2EthereumNetworks.cs
+++ server/src/FunFair.Labs.ScalingEthereum.Contracts/Networks/Layer2EthereumNetworks.cs
@@ -13,20 +13,20 @@ namespace FunFair.Labs.ScalingEthereum.Contracts.Networks
         ///     The Optimism Mainnet
         /// </summary>
         public static EthereumNetwork OptimismMainNet { get; } =
-            new(networkId: 1337, chainId: 10, name: @"OptimismMainNet", nativeCurrency: "OETH", isProduction: true, isStandalone: false, isPublic: true, fixedGasPrice: GasPrice.FromGwei(1),
-                blockExplorer: new BlockExplorer(name: "Surge",
-                                                 new Uri("https://mainnet-l2-explorer.surge.sh/"),
-                                                 new Uri("https://mainnet-l2-explorer.surge.sh/address/"),
-                                                 new Uri("https://mainnet-l2-explorer.surge.sh/tx/")));
+            new(networkId: 1337, chainId: 10, name: @"OptimismMainNet", blockFrequency: BlockFrequency.ON_DEMAND, nativeCurrency: "OETH", isProduction: true, isStandalone: false, isPublic: true,
+                fixedGasPrice: GasPrice.FromGwei(1), blockExplorer: new BlockExplorer(name: "Surge",
+                                                                                      new Uri("https://mainnet-l2-explorer.surge.sh/"),
+                                                                                      new Uri("https://mainnet-l2-explorer.surge.sh/address/"),
+                                                                                      new Uri("https://mainnet-l2-explorer.surge.sh/tx/")));
 
         /// <summary>
         ///     The Optimism Kovan Testnet
         /// </summary>
         public static EthereumNetwork OptimismKovan { get; } =
-            new(networkId: 1337, chainId: 69, name: @"OptimismKovan", nativeCurrency: "OKOVETH", isProduction: true, isStandalone: false, isPublic: true, fixedGasPrice: GasPrice.FromGwei(1),
-                blockExplorer: new BlockExplorer(name: "Surge",
-                                                 new Uri("https://kovan-l2-explorer.surge.sh/"),
-                                                 new Uri("https://kovan-l2-explorer.surge.sh/address/"),
-                                                 new Uri("https://kovan-l2-explorer.surge.sh/tx/")));
+            new(networkId: 1337, chainId: 69, name: @"OptimismKovan", blockFrequency: BlockFrequency.ON_DEMAND, nativeCurrency: "OKOVETH", isProduction: true, isStandalone: false, isPublic: true,
+                fixedGasPrice: GasPrice.FromGwei(1), blockExplorer: new BlockExplorer(name: "Surge",
+                                                                                      new Uri("https://kovan-l2-explorer.surge.sh/"),
+                                                                                      new Uri("https://kovan-l2-explorer.surge.sh/address/"),
+                                                                                      new Uri("https://kovan-l2-explorer.surge.sh/tx/")));
     }
 }
