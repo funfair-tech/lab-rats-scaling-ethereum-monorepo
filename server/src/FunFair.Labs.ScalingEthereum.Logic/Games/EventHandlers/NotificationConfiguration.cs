a5dc1167f5498d374d5eac86027d14d4dc391018
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-04-29 15:18:51 +0100

    Return 1 history item - any more is too much for the client to interpret in a sane way

--- server/src/FunFair.Labs.ScalingEthereum.Logic/Games/EventHandlers/NotificationConfiguration.cs
+++ server/src/FunFair.Labs.ScalingEthereum.Logic/Games/EventHandlers/NotificationConfiguration.cs
@@ -8,6 +8,6 @@
         /// <summary>
         ///     Maximum number of history items to get.
         /// </summary>
-        public const int MaxHistoryItems = 10;
+        public const int MaxHistoryItems = 1;
     }
 }
