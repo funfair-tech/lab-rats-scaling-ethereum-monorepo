bdabb55787ded06be0f6c1b5f964a0f935841025
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-05-05 14:45:31 +0100

    Changed timings of bets

--- server/src/FunFair.Labs.ScalingEthereum.Logic/Games/GameRoundParameters.cs
+++ server/src/FunFair.Labs.ScalingEthereum.Logic/Games/GameRoundParameters.cs
@@ -10,12 +10,12 @@ namespace FunFair.Labs.ScalingEthereum.Logic.Games
         /// <summary>
         ///     Length of a round.
         /// </summary>
-        public static TimeSpan RoundDuration { get; } = TimeSpan.FromSeconds(value: 20);
+        public static TimeSpan RoundDuration { get; } = TimeSpan.FromSeconds(value: 10);
 
         /// <summary>
         ///     Length of the betting close period.
         /// </summary>
-        public static TimeSpan BettingCloseDuration { get; } = TimeSpan.FromSeconds(value: 5);
+        public static TimeSpan BettingCloseDuration { get; } = TimeSpan.FromSeconds(value: 1);
 
         /// <summary>
         ///     The amount of time to wait between rounds, so the client has time to display
